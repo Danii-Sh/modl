8d5cb434b1eca85bdff1fe93958f1c8858a23304
Danii-Sh <83266030+Danii-Sh@users.noreply.github.com>
2023-07-27 21:03:54 +0330

    Update tstDemo.py

--- tstDemo.py
+++ tstDemo.py
@@ -17,12 +17,12 @@ import matplotlib.pyplot as plt
 import supportingFunctions as sf
 
 cwd=os.getcwd()
-tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
 
 #%% choose a model from savedModels directory
 
 #subDirectory='14Mar_1105pm'
-subDirectory='04Jun_0356pm_5L_10K_50E_AG'
+subDirectory='21Jul_1035PM_5L_1K_50E_AG'
 #%%Read the testing data from dataset.hdf5 file
 
 #tstOrg is the original ground truth
@@ -30,7 +30,7 @@ subDirectory='04Jun_0356pm_5L_10K_50E_AG'
 #tstCsm: this is coil sensitivity maps
 #tstMask: it is the undersampling mask
 
-tstOrg,tstAtb,tstCsm,tstMask=sf.getTestingData()
+tstOrg,tstAtb,tstA = sf.getTestingData()
 
 #you can also read more testing data from dataset.hdf5 (see readme) file using the command
 #tstOrg,tstAtb,tstCsm,tstMask=sf.getData('testing',num=100)
@@ -41,20 +41,20 @@ print ('Now loading the model ...')
 modelDir= cwd+'/savedModels/'+subDirectory #complete path
 rec=np.empty(tstAtb.shape,dtype=np.complex64) #rec variable will have output
 
-tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
 loadChkPoint=tf.train.latest_checkpoint(modelDir)
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
-with tf.Session(config=config) as sess:
-    new_saver = tf.train.import_meta_graph(modelDir+'/modelTst.meta')
+with tf.compat.v1.Session(config=config) as sess:
+    new_saver = tf.compat.v1.train.import_meta_graph(modelDir+'/modelTst.meta')
     new_saver.restore(sess, loadChkPoint)
-    graph = tf.get_default_graph()
+    graph = tf.compat.v1.get_default_graph()
     predT =graph.get_tensor_by_name('predTst:0')
-    maskT =graph.get_tensor_by_name('mask:0')
+    AT =graph.get_tensor_by_name('A:0')
     atbT=graph.get_tensor_by_name('atb:0')
-    csmT   =graph.get_tensor_by_name('csm:0')
-    wts=sess.run(tf.global_variables())
-    dataDict={atbT:tstAtb,maskT:tstMask,csmT:tstCsm }
+    print ('in test', atbT.shape,tstAtb.shape, AT.shape, tstA.shape)
+    wts=sess.run(tf.compat.v1.global_variables())
+    dataDict={atbT:tstAtb,AT:tstA }
     rec=sess.run(predT,feed_dict=dataDict)
 
 rec=sf.r2c(rec.squeeze())
@@ -63,10 +63,14 @@ print('Reconstruction done')
 #%% normalize the data for calculating PSNR
 
 print('Now calculating the PSNR (dB) values')
+print ('PSNR', tstOrg.shape, tstAtb.shape,rec.shape, tstOrg.dtype)
 
-normOrg=sf.normalize01( np.abs(tstOrg))
+
+normOrg=sf.normalize01( np.abs((tstOrg.astype('float32'))))
 normAtb=sf.normalize01( np.abs(sf.r2c(tstAtb)))
 normRec=sf.normalize01(np.abs(rec))
+print ('PSNR', normOrg.shape,normAtb.shape,normRec.shape)
+
 
 psnrAtb=sf.myPSNR(normOrg,normAtb)
 psnrRec=sf.myPSNR(normOrg,normRec)
@@ -79,19 +83,19 @@ print ('*****************')
 #%% Display the output images
 plot= lambda x: plt.imshow(x,cmap=plt.cm.gray, clim=(0.0, .8))
 plt.clf()
-plt.subplot(141)
-plot(np.fft.fftshift(tstMask[0]))
-plt.axis('off')
-plt.title('Mask')
-plt.subplot(142)
+# plt.subplot(141)
+# plot(np.fft.fftshift(tstMask[0]))
+# plt.axis('off')
+# plt.title('Mask')
+plt.subplot(131)
 plot(normOrg)
 plt.axis('off')
 plt.title('Original')
-plt.subplot(143)
+plt.subplot(132)
 plot(normAtb)
 plt.title('Input, PSNR='+str(psnrAtb.round(2))+' dB' )
 plt.axis('off')
-plt.subplot(144)
+plt.subplot(133)
 plot(normRec)
 plt.title('Output, PSNR='+ str(psnrRec.round(2)) +' dB')
 plt.axis('off')
